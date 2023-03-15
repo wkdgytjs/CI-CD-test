7b5f3547299c7e908cf90ba31b8fa6a297352964
wkdgytjs <gytjs513563@gmail.com>
2023-03-15 11:56:50 +0900

    cicd test

---
+++ src/main/java/org/spring/cicdproject/CicdprojectApplication.java
@@ -0,0 +1,13 @@
+package org.spring.cicdproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/spring/cicdproject/CicdprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.cicdproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
